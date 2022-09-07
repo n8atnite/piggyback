f47728763156a33b95e0367d7280202be43fdab7
Nate Atkinson <atkies@ucla.edu>
2022-09-06 23:42:03 -0700

    css baseline

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>  
-    <React.StrictMode>
+    <CssBaseline />
     <App />
-    </React.StrictMode>
   </ThemeProvider>,
 );
 
