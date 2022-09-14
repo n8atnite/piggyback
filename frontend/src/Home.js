9a8c8a63629b2b8a6d5673086698fe210b5a0e23
factormrp <factormrp@gmail.com>
2022-09-13 23:57:43 -0700

    Added home page background + Cloud component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,17 +1,56 @@
+import Cloud from './Cloud';
 import * as React from 'react';
-import { Box, Paper } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import ResponsiveAppBar from './ResponsiveAppBar';
 
 export default function Home() {
     return (
       <>
-        <Paper elevation={1}>
-          <ResponsiveAppBar/>
-        </Paper>
-        <Box sx={{ height: '1200px'}}>
-          <p sx={{ color: 'text.primary', fontSize: 34 }}>
-            Call to Action
-          </p>
+        <Box sx={{ backgroundColor: '#5394fc' ,height: '1920px' }}
+        >
+          <ResponsiveAppBar />
+          <br></br>
+          <Grid container spacing={2} sx={{ position: 'absolute',
+            left: '100px' }} 
+          >
+            <Grid xs={2}>
+              <Cloud />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={300} />
+            </Grid>
+            <Grid xs={4}>
+              <Cloud height={500} width={500} />
+            </Grid>
+            <Grid xs={4}>
+              <Cloud height={400} width={300} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud width={500} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={300} width={300} />
+            </Grid>
+            <Grid xs={4}>
+              <Cloud text='peep merch' height={300} width={500}
+                shift={-100} scoot={-65} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={200} width={150} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={300} width={300} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={200} width={150} />
+            </Grid>
+            <Grid xs={4}>
+              <Cloud height={400} width={300} />
+            </Grid>
+            <Grid xs={2}>
+              <Cloud height={200} width={150} />
+            </Grid>
+          </Grid>
         </Box>
       </>
     );
